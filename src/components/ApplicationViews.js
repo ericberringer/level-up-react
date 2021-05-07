bedde370b234ce565cbe8eaa8f2b0e4f6b2c786b
ericberringer <eric.berringer@gmail.com>
2021-05-07 10:01:12 -0500

    events show up on the dom.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { EventList } from "./event/EventList.js"
 import { EventProvider } from "./event/EventProvider.js"
 import { GameList } from "./game/GameList.js"
 import { GameProvider } from "./game/GameProvider.js"
