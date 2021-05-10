5194dc359013da85ec8c0a2aa0989d5d1387de97
ericberringer <eric.berringer@gmail.com>
2021-05-10 14:08:18 -0500

    events now include a join button that joins the logged in user to that event as an attendee, commented out an attendee variable so need to sort that out.

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -4,7 +4,7 @@ import { EventContext } from "./EventProvider.js"
 
 export const EventList = (props) => {
 
-    const { events, getEvents } = useContext(EventContext)
+    const { events, getEvents, joinEvent } = useContext(EventContext)
 
     const history = useHistory()
 
@@ -16,32 +16,27 @@ export const EventList = (props) => {
         <article className="events">
             <header className="events__header">
                 <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
             </header>
             {
                 events.map(event => {
+                    // const attending = profile.events.some(evt => evt.id === event.id)
                     return <section key={event.id} className="registration">
                         <div className="registration__game">{event.game.title}</div>
                         <div>{event.description}</div>
                         <div>
-                            {
-                                new Date(event.date).toLocaleDateString("en-US",
-                                {
-                                    weekday: 'long',
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric'
-                                })
-                            }
-                            @ {event.time}
+                            {event.date} @ {event.time}
                         </div>
+                        <button className="btn btn-2"
+                                onClick={() => joinEvent(event.id)}
+                        >Join</button>
                     </section>
                 })
             }
-            <button className="btn btn-2 btn-sep icon-create"
-            onClick={() => {
-            history.push({ pathname: "/events/new" })
-            }}
-            >Register New Event</button>
         </article >
     )
 }
